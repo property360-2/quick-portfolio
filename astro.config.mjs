6fa597d38eec5c4e98c70a3cb65f9f9783e23465
property360-2 <junalvior21@gmail.com>
2026-04-22 22:10:22 +0800

    some error fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://property360-2.github.io',
   base: '/quick-portfolio',
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
